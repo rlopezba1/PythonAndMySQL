4d2598ce0dfb6f617ecf6913573909fc89d3f329
rlopezba1 <rubenlopezb@gmail.com>
2021-10-15 10:04:49 +0000

    created a new table and insert data

---
+++ mysql-from-python.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+import pymysql
+
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
+finally:
+    # Close the connection, regardless of whether or not the above was successful
+    connection.close()
